66e9800b2285513ba9bd931a9cbf85a907f862d6
BuckForth <45402924+BuckForth@users.noreply.github.com>
2018-11-27 13:54:39 -0600

    Needed Them Libs

---
+++ Unnamed Defence Game/Assets/Resources/Scripts/Blocks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Blocks
+{
+	public static Block[] blocks = 
+	{
+		new ColumnBlock("Grass",1.1f,1.2f,0,0,1),
+		new Block("Dirt",1.0f,1.0f,0,2),
+		new Block("Stone",5.0f,3.0f,0,3),
+		new Block("Logs",2.0f,1.5f,5.0f,4),
+		new Block("Wood",2.0f,1.5f,3.0f,5),
+		new Block("Bedrock",float.MaxValue,float.MaxValue,0f,6),
+		new Block("Ore",5.0f,3.0f,0,7),
+		new Block("StoneWall",7.5f,3.5f,0f,8)
+		
+	};
+}
+
---
+++ Unnamed Defence Game/Assets/Resources/Scripts/Entities.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Entities
+{
+	public static Entity[] entities = 
+	{
+		new Entity()
+		
+	};
+}
+
